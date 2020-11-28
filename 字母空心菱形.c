843681e165e4de91f1d277343f94aa89b469f068
GODBENGAY <74138149+GODBENGAY@users.noreply.github.com>
2020-11-28 11:20:59 +0800

    Add files via upload

---
+++ 字母空心菱形.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+int main()
+{
+	int i,n,n1,j=1,m,n2,ji1,ji2;
+	char first;
+	scanf("%c%d",&first,&n);
+	for (i=1,ji1=first-1;i<=n;i++)
+	{
+		if ((ji1+i)>'Z') ji1='A'-i;
+		for (j=1;j<=(n-i);j++)
+		{printf(" ");} j=1;
+		printf("%c",ji1+i);
+		for (m=1;m<=(2*i-3);m++)
+		{printf(" ");} m=1;
+		if ((i*2-3)>0)
+		printf("%c",ji1+i);
+		printf("\n");
+	}
+	n2=n-1;
+	for (i=1,ji2=ji1+n-1;i<=n2;i++)
+	{
+		{for (j=1;j<=i;j++)
+		{printf(" ");}}
+		if (ji2<'A') ji2='Z';
+		printf("%c",ji2);
+		for (m=1;m<=(2*n2-2*i-1);m++)
+        {printf(" ");}	m=1;
+		if ((2*n2-2*i-1)>0)
+		printf("%c",ji2);
+		printf("\n");
+		ji2--;
+	}
+}
---
+++ 递归颠倒数字.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h> 
+#include <stdlib.h>   
+int reverse();
+int main( )    
+{   char str[100];    
+    int start, end;    
+    gets(str);    
+    scanf("%d%d", &start, &end);    
+    reverse( str, start, end );    
+    printf("%s\n", str);    
+    return 0;    
+}  
+    int reverse(char *str,int start,int end)
+  {
+  	char c;
+  	c=str[start];  str[start]=str[end];  str[end]=c;
+  	start++;
+  	end--;
+  	if (end>start)   reverse( str, start, end );  
+  }
